d0a44f6bf5d7356367fa38666053dba586695e86
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-05-21 23:32:22 -0300

    cleanup omeg/conf/setup.py a little bit

--- omeg/conf/setup.py
+++ omeg/conf/setup.py
@@ -7,12 +7,8 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URI")
-        or r"sqlite:///" + basedir + r"/omeg/data/sqlite.db"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
