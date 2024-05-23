657b8fbb4dccff54c81e67d21f7208a1be175eb0
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-05-23 10:34:30 -0300

    navbar is ready

--- omeg/user/routes.py
+++ omeg/user/routes.py
@@ -31,26 +31,124 @@ def professor_dashboard(taxnr):
         return redirect(url_for("bp_home_routes.home"))
 
 
+@bp_user_routes.route("/professor/<taxnr>/dates")
+def dates(taxnr):
+    if taxnr == current_user.taxnr:
+        professor = db.first_or_404(
+            sa.select(Professor).where(Professor.taxnr == taxnr)
+        )
+        return render_template(
+            "dates.html",
+            payload=payload,
+            professor=professor,
+            taxnr=taxnr,
+        )
+
+
 @bp_user_routes.route("/professor/<taxnr>/inep")
 @login_required
 def inep(taxnr):
     if taxnr == current_user.taxnr:
+        professor = db.first_or_404(
+            sa.select(Professor).where(Professor.taxnr == taxnr)
+        )
         schools = db.session.scalars(
             sa.select(School).order_by(School.city)
         ).all()
         return render_template(
             "schools.html",
             payload=payload,
+            professor=professor,
             schools=schools,
         )
     else:
         return redirect(url_for("bp_home_routes.home"))
 
 
+@bp_user_routes.route("/professor/<taxnr>/students")
+@login_required
+def registered_students(taxnr):
+    if taxnr == current_user.taxnr:
+        professor = db.first_or_404(
+            sa.select(Professor).where(Professor.taxnr == taxnr)
+        )
+        students = (
+            db.session.query(
+                Student.cpfnr,
+                Student.fname,
+                Student.birth,
+                Student.email,
+                Enrollment.inep,
+                Enrollment.roll,
+            )
+            .where(
+                Enrollment.taxnr == taxnr,
+                Enrollment.cpfnr == Student.cpfnr,
+                Enrollment.year == payload["edition"],
+            )
+            .order_by(
+                Enrollment.roll,
+                Student.fname,
+            ).all()
+        )
+        return render_template(
+            "registered_students.html",
+            payload=payload,
+            professor=professor,
+            taxnr=taxnr,
+            students=students,
+        )
+
+
+@bp_user_routes.route(
+    "/professor/<taxnr>/new_student", methods=["GET", "POST"]
+)
+@login_required
+def student_registration(taxnr):
+    if taxnr == current_user.taxnr:
+        professor = db.first_or_404(
+            sa.select(Professor).where(Professor.taxnr == taxnr)
+        )
+        form = student_registration_form()
+        if form.validate_on_submit():
+            student = Student(
+                cpfnr=cpf_strfmt(form.cpfnr.data),
+                fname=form.fname.data,
+                birth=date_strfmt(form.birth.data),
+                email=form.email.data,
+            )
+            enrollment = Enrollment(
+                cpfnr=cpf_strfmt(form.cpfnr.data),
+                taxnr=taxnr,
+                inep=form.inep.data,
+                year=payload["edition"],
+                roll=form.roll.data,
+                gift="N",
+            )
+            db.session.add(student)
+            db.session.commit()
+            db.session.add(enrollment)
+            db.session.commit()
+            flash("Estudante cadastrado com sucesso!")
+            return redirect(
+                url_for("bp_user_routes.professor_dashboard", taxnr=taxnr)
+            )
+    return render_template(
+        "student_registration.html",
+        payload=payload,
+        professor=professor,
+        taxnr=taxnr,
+        form=form,
+    )
+
+
 @bp_user_routes.route("/professor/<taxnr>/extract")
 @login_required
 def students_extract(taxnr):
     if taxnr == current_user.taxnr:
+        professor = db.first_or_404(
+            sa.select(Professor).where(Professor.taxnr == taxnr)
+        )
         extract1 = {
             inep[0]: {
                 i: db.session.query(Enrollment)
@@ -87,75 +185,10 @@ def students_extract(taxnr):
         return render_template(
             "students_extract.html",
             payload=payload,
+            professor=professor,
             extract1=extract1,
             extract2=extract2,
             extract3=extract3,
             extract4=extract4,
             extract5=extract5,
         )
-
-
-@bp_user_routes.route("/professor/<taxnr>/students")
-@login_required
-def registered_students(taxnr):
-    if taxnr == current_user.taxnr:
-        students = (
-            db.session.query(
-                Student.cpfnr,
-                Student.fname,
-                Student.birth,
-                Student.email,
-                Enrollment.inep,
-                Enrollment.roll,
-            )
-            .where(
-                Enrollment.taxnr == taxnr,
-                Enrollment.cpfnr == Student.cpfnr,
-                Enrollment.year == payload["edition"],
-            )
-            .order_by(
-                Enrollment.roll,
-                Student.fname,
-            ).all()
-        )
-        return render_template(
-            "registered_students.html",
-            payload=payload,
-            taxnr=taxnr,
-            students=students,
-        )
-
-
-@bp_user_routes.route(
-    "/professor/<taxnr>/new_student", methods=["GET", "POST"]
-)
-@login_required
-def student_registration(taxnr):
-    if taxnr == current_user.taxnr:
-        form = student_registration_form()
-        if form.validate_on_submit():
-            student = Student(
-                cpfnr=cpf_strfmt(form.cpfnr.data),
-                fname=form.fname.data,
-                birth=date_strfmt(form.birth.data),
-                email=form.email.data,
-            )
-            enrollment = Enrollment(
-                cpfnr=cpf_strfmt(form.cpfnr.data),
-                taxnr=taxnr,
-                inep=form.inep.data,
-                year=payload["edition"],
-                roll=form.roll.data,
-                gift="N",
-            )
-            db.session.add(student)
-            db.session.commit()
-            db.session.add(enrollment)
-            db.session.commit()
-            flash("Estudante cadastrado com sucesso!")
-            return redirect(
-                url_for("bp_user_routes.professor_dashboard", taxnr=taxnr)
-            )
-    return render_template(
-        "student_registration.html", payload=payload, taxnr=taxnr, form=form
-    )
